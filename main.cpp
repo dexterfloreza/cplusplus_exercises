403a9a7b272fc92d8cbbeede86e359c09c56741f
dexterfloreza <deryanfloreza@outlook.com>
2025-10-16 16:44:13 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+// This is a hello world program.
+int main() {
+    // This is a program that displays mixed data types and arithmetic operations
+    cout << "Hello World!";
+    return 0;
+}
